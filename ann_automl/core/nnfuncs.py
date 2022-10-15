29a66c703c91ce34d9ac05307017424814c541f8
Kalachev Gleb <gleb.kalachev@yandex.ru>
2022-10-16 02:29:42 +0300

    Добавил функции и классы для оптимизации по сетке

--- ann_automl/core/nnfuncs.py
+++ ann_automl/core/nnfuncs.py
@@ -1,10 +1,15 @@
+import itertools
+import math
 import os
 import time
+from collections import defaultdict
 
 import keras
+import numpy as np
 import pandas as pd
 
 from . import db_module
+from .solver import printlog
 
 _data_dir = 'data'
 _db_file = 'tests.sqlite'
@@ -46,7 +51,8 @@ hyperparameters = {
     }, 'default': 'Adam', 'name': "оптимизатор"},
     'learning_rate': {'type': 'float', 'range': [1e-5, 1e-1], 'default': 1e-3, 'step': 2, 'scale': 'log',
                       'name': "скорость обучения"},
-    'decay': {'type': 'float', 'range': [0, 1], 'default': 0.0, 'name': 'декремент скорости обучения'},
+    'decay': {'type': 'float', 'range': [0, 1], 'default': 0.0, 'step': 0.01, 'scale': 'lin',
+              'name': 'декремент скорости обучения'},
     'activation': {'type': 'str', 'values': ['softmax', 'elu', 'selu', 'softplus', 'softsign', 'relu', 'tanh',
                                              'sigmoid', 'hard_sigmoid', 'linear'],
                    'default': 'relu', 'name': 'функция активации'},
@@ -182,8 +188,8 @@ class ExperimentHistory:
 
         self.history = pd.DataFrame(columns=['Index', 'task_type', 'objects', 'exp_name', 'pipeline', 'last_layers',
                                              'augmen_params', 'loss', 'metrics', 'epochs', 'stop_criterion', 'data',
-                                             'optimizer', 'batch_size', 'lr', 'metric_test_value',
-                                             'train_subdir', 'time_stat', 'total_time', 'Additional_params'])
+                                             'optimizer', 'batch_size', 'learning_rate', 'metric_test_value',
+                                             'train_subdir', 'time_stat', 'total_time', 'additional_params'])
 
         self.save()
 
@@ -200,19 +206,18 @@ class ExperimentHistory:
                 'loss': params['loss'],  # функция потерь
                 'metrics': params['metrics'],  # метрика, по которой оценивается качество модели
                 'epochs': params['epochs'],  # количество эпох обучения
-                'stop_criterion': params['stop_criterion'],
-                # критерий остановки обучения (TODO: не используется, исправить!!!)
+                'stop_criterion': params['stop_criterion'],  # критерий остановки обучения (TODO: не используется, исправить!!!)
                 'data': params['data'],  # набор данных, на котором проводится обучение
 
                 'optimizer': params['optimizer'],  # оптимизатор
                 'batch_size': params['batch_size'],  # размер батча
-                'lr': params['lr'],  # скорость обучения
+                'learning_rate': params['learning_rate'],  # скорость обучения
 
                 'metric_test_value': metric,  # значение метрики на тестовой выборке
                 'train_subdir': train_subdir,  # папка, в которой хранятся результаты текущего обучения
                 'time_stat': time_stat,  # список длительностей всех эпох обучения
                 'total_time': total_time,  # общее время обучения
-                'Additional_params': [{}]})
+                'additional_params': [{}]})
 
         self.history.append(row, ignore_index=True)
         if save:
@@ -233,7 +238,7 @@ class ExperimentHistory:
         best_model = self.get_best_model()
         return {'optimizer': best_model['optimizer'],
                 'batch_size': best_model['batch_size'],
-                'lr': best_model['lr']}
+                'learning_rate': best_model['learning_rate']}
 
 
 class StopFlag:
@@ -272,7 +277,8 @@ def fit_model(model, hparams, generators, cur_subdir, history=None, stop_flag=No
     List[float]
         Достигнутые значения метрик на тестовой выборке во время обучения
     """
-    optimizer, lr = hparams['optimizer'], hparams['lr']
+
+    optimizer, lr = hparams['optimizer'], hparams['learning_rate']
     opt_args = ['decay'] + hyperparameters['optimizer']['values'][optimizer].get('params', [])
     kwargs = {arg: hparams[arg] for arg in opt_args if arg in hparams}
     optimizer = getattr(keras.optimizers, optimizer)(learning_rate=lr, **kwargs)
@@ -305,3 +311,222 @@ def fit_model(model, hparams, generators, cur_subdir, history=None, stop_flag=No
         history.add_row(hparams, scores[1], cur_subdir, c_t.times, c_t.total_time, save=True)
 
     return scores
+
+
+def create_and_train_model(hparams, data, cur_subdir, history=None, stop_flag=None):
+    """
+    Parameters
+    ----------
+    hparams: dict
+        словарь с гиперпараметрами обучения
+    data: tuple
+        кортеж из трех генераторов: train, val, test
+    cur_subdir: str
+        папка, в которой хранятся результаты текущего обучения
+    history: Optional[ExperimentHistory]
+        история экспериментов
+    stop_flag: Optional[StopFlag]
+        флаг, с помощью которой можно остановить обучение из другого потока
+    Returns
+    -------
+    List[float]
+        Достигнутые значения метрик на тестовой выборке во время обучения
+    """
+    model = create_model(hparams['pipeline'], hparams['last_layers'])
+    generators = create_generators(model, data, hparams['augmen_params'], hparams['batch_size'])
+    return fit_model(model, hparams, generators, cur_subdir, history=history, stop_flag=stop_flag)
+
+
+grid_hparams_space = {  # гиперпараметры, которые будем перебирать по сетке
+    # TODO: объединить как-то с hyperparameters
+    'optimizer': {'values': [
+        ('Adam', {'params': ['amsgrad', 'beta_1', 'beta_2', 'epsilon']}),
+        ('SGD', {'scale': {'learning_rate': 10}, 'params': ['nesterov', 'momentum']}),
+        ('RMSprop', {'params': ['rho', 'epsilon', 'momentum', 'centered']}),
+    ]},
+    # для каждого оптимизатора указывается, как другие гиперпараметры должны масштабироваться при смене оптимизатора
+    'batch_size': {'range': [1, 1024], 'default': 1024, 'step': 2, 'scale': 'log', 'type': 'int'},
+    'learning_rate': {'range': [0.000125, 0.064], 'default': 0.001, 'step': 2, 'scale': 'log', 'type': 'float'},
+    'lr/batch_size': {'range': [0.00000125, 0.00128], 'default': 0.001, 'step': 2, 'scale': 'log', 'type': 'float'},
+    # только один из двух параметров может быть задан: learning_rate или lr/batch_size
+    'decay': {'type': 'float', 'range': [1/2**5, 1], 'default': 0.0, 'step': 2, 'scale': 'log', 'zero_point': 1},
+
+    # conditonal params
+    'amsgrad': {'values': [True, False], 'default': False, 'cond': True},
+    'nesterov': {'values': [True, False], 'default': True, 'cond': True},
+    'centered': {'values': [True, False], 'default': False, 'cond': True},
+
+    'beta_1': {'range': [0.5, 0.999], 'default': 0.9, 'cond': True, 'step': 2, 'scale': 'loglog'},
+    'beta_2': {'range': [0.5, 0.9999], 'default': 0.999, 'cond': True, 'step': 2, 'scale': 'loglog'},
+    'rho': {'range': [0.5, 0.9999], 'default': 0.9, 'cond': True, 'step': 2, 'scale': 'loglog'},
+    'epsilon': {'range': [1e-8, 1], 'default': 1e-7, 'cond': True, 'step': 10, 'scale': 'log'},
+    'momentum': {'range': [0, 1], 'default': 0.0, 'cond': True, 'step': 0.1, 'scale': 'lin'},
+}
+
+
+def param_values(range=None, default=None, values=None, step=None, scale=None, zero_point=None, type=None, **kwargs):
+    if range is not None:
+        if scale == 'log':
+            back = round(math.log(range[0]/default, step))
+            forward = round(math.log(range[1]/default, step))
+            res = [default * step ** i for i in range(back, forward + 1)]
+        elif scale == 'loglog':
+            back = round(math.log(math.log(range[0])/math.log(default), step))
+            forward = round(math.log(math.log(range[1])/math.log(default), step))
+            res = [default ** (step ** i) for i in range(back, forward + 1)]
+        elif scale == 'lin':
+            back = round((range[0] - default) / step)
+            forward = round((range[1] - default) / step)
+            res = [default + step * i for i in range(back, forward + 1)]
+        else:
+            raise ValueError(f'Unknown scale {scale}')
+        if type == 'int':
+            res = [int(round(x)) for x in res]
+        if zero_point:
+            res = [0] + res
+        return res
+    elif values is not None:
+        return list(values)
+    else:
+        raise ValueError('Either `range` or `values` should be specified')
+
+
+class HyperParamGrid:
+    def __init__(self, hparams, grid_params):
+        """
+        Parameters
+        ----------
+        hparams: dict
+            Словарь с текущими значениями гиперпараметров
+        grid_params: List
+            Набор гиперпараметров, которые будут подбираться
+        """
+        self.hparams = hparams
+        self.grid_params = grid_params
+        self.fixed_params = [p for p in hparams if p not in grid_params]
+        self.param_control = {p: grid_hparams_space[p]['values'] for p in grid_params
+                              if 'values' in grid_hparams_space[p] and isinstance(grid_hparams_space[p]['values'], dict)}
+        active = set()
+        for p in self.fixed_params:
+            v = hparams[p]
+            active.update(self.param_control.get(p, {}).get(v, ()))
+        self.active = {p for p in self.grid_params if not grid_hparams_space[p].get('cond', False) or p in active}
+
+        self.axis = [param_values(**grid_hparams_space[param]) for param in grid_params]
+
+    def remove_inactive(self, point):  # replaces inactive dependent params with None
+        active = {*self.active}
+        for p, i, ax in zip(self.grid_params, point, self.axis):
+            if p in active:
+                active.update(self.param_control.get(p, {}).get(ax[i], ()))
+
+        return tuple(x if p in active else None for p, x in zip(self.grid_params, point))
+
+    def __call__(self, point):
+        key = self.remove_inactive(point)
+        pt_params = {p: ax[v] for p, v, ax in zip(self.grid_params, key, self.axis) if v is not None}
+        res = {**self.hparams, **pt_params}
+
+        # apply parameter scaling depending on other parameters
+        for p, pp in self.param_control.items():
+            if p in res:
+                for ppp, s in pp.get(res[p], {}).items():
+                    if ppp in res:
+                        res[ppp] *= s
+        return res
+
+
+def neighborhood_gen(c, shape, cat_axis, r, metric):
+    """
+    Функция возвращает все точки в окрестности центра, которые не выходят за границы сетки
+    :param c: центр окрестности
+    :param shape: размеры сетки
+    :param cat_axis: типы величин по осям (0 -- числовая, 1 -- категориальная)
+    :param r: радиус окрестности
+    :param metric: метрика расстояния на сетке ('max' -- максимум, 'l1' -- манхэттенское расстояние)
+    :returns: генератор точек окрестности точки c
+    """
+    if metric == 'max':
+        ranges = [range(shape[i]) if cat_axis[i] else range(max(0, c[i] - r), min(shape[i], c[i] + r + 1)) for i in range(len(c))]
+        return (v for v in itertools.product(*ranges) if v != c)
+    elif metric == 'l1':
+        def yield_rec(i, ri):
+            if len(c) == i or ri == 0:
+                yield c[i:]
+            else:
+                if cat_axis[i]:
+                    for j in range(shape[i]):
+                        for v in yield_rec(i + 1, ri - (j != c[i])):
+                            yield (j,) + v
+                else:
+                    for j in range(max(0, c[i] - ri), min(shape[i], c[i] + ri + 1)):
+                        for v in yield_rec(i + 1, ri - abs(j - c[i])):
+                            yield (j,) + v
+
+        return (v for v in yield_rec(0, r) if v != c)
+    else:
+        raise ValueError(f'Unknown metric: {metric}')
+
+
+def grid_search_gen(grid_size, axis_types, func, gridmap, start_point='random', grid_metric='l1', radius=1):
+    """
+    Оптимизирует функцию на сетке жадным алгоритмом.
+    На каждом шаге выбирается точка с максимальным значением функции в окрестности текущей точки.
+    Если максимум достигается в центре окрестности, оптимизация на этом шаге завершается.
+
+    Parameters
+    ----------
+    grid_size: tuple
+        Размер сетки, на которой производится оптимизация. Каждый элемент кортежа - это количество точек в соответствующей оси.
+    axis_types: tuple
+        Типы величин по осям ('num' -- числовая, 'cat' -- категориальная)
+    func: function
+        Функция, которую нужно оптимизировать.
+    gridmap: function
+        Функция, которая преобразует точку сетки в кортеж (key, args, kwargs), где
+        key - ключ для кэширования, args и kwargs - аргументы функции func.
+    start_point:    Union[tuple, str]
+        Начальная точка. Если 'random', то начальная точка выбирается случайно.
+    grid_metric: str
+        Метрика, по которой определяется расстояние между точками сетки ('l1' или 'max').
+    radius: int
+        Радиус окрестности, в которой производится поиск лучшей точки.
+
+    Yields
+    -------
+    tuple
+        Тройка, в которой первый элемент -- кортеж с координатами текущей точки,
+        второй элемент -- значение функции в этой точке, третий элемент -- является ли точка локальным максимумом.
+    """
+    if start_point == 'random':
+        start_point = tuple(np.random.randint(0, grid_size[i]) for i in range(len(grid_size)))
+    else:
+        start_point = tuple(start_point)
+
+    cur_point = start_point
+    key, args, kwargs = gridmap(cur_point)
+    cur_value = func(*args, **kwargs)
+    cache = {key: cur_value}
+    yield cur_point, cur_value, False
+
+    while True:
+        printlog(f'Current point: {cur_point}, value: {cur_value}')
+        best_point = cur_point
+        best_value = cur_value
+        for point in neighborhood_gen(cur_point, grid_size, axis_types, radius, grid_metric):
+            key, args, kwargs = gridmap(point)
+            if key is None:  # если точка не входит в область определения функции
+                continue
+            if key not in cache:
+                cache[key] = func(*args, **kwargs)
+                yield point, cache[key], False
+            if cache[key] > best_value:
+                best_point = point
+                best_value = cache[key]
+
+        if best_point == cur_point:
+            yield cur_point, cur_value, True
+            break
+
+        cur_point = best_point
+        cur_value = best_value
