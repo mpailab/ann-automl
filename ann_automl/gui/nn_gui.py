c4b3006e615a786db89c78f0d09c4d81a85f0e3a
Kalachev Gleb <gleb.kalachev@yandex.ru>
2022-11-18 23:14:01 +0300

    Исправлена ошибка добавления нового датасета через GUI

--- ann_automl/gui/nn_gui.py
+++ ann_automl/gui/nn_gui.py
@@ -399,7 +399,6 @@ class Database(Window):
         )
 
 
-
 class DatasetLoader(Window):
 
     next_window = param.Selector(default='Database', objects=['Database'])
@@ -483,7 +482,7 @@ class DatasetLoader(Window):
         self.dataset_checker.text = ""
 
         try:
-            cur_db().fill_coco(
+            cur_db().fill_in_coco_format(
                 self.anno_file_setter.value,
                 self.dataset_dir_setter.value,
                 ds_info={
